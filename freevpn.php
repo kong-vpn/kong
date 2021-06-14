5bbd38142d855d286c9395cb9e14e96f18fa2ac4
kong-vpn <81752336+kong-vpn@users.noreply.github.com>
2021-06-14 18:28:27 +0700

    Add files via upload

---
+++ freevpn.php
@@ -0,0 +1,48 @@
+{"Version":"1.9",
+"Url":"https://github.com/kong-vpn/kong/raw/main/freevpn.zip" ,
+"Changelog":"  
+ยินดีต้อนรับสู่ Store VPN
+รายการอัพเดท 08.มิ.ย.2564
+
+รายการอัพเดทวันนี้
+
+ไฟล์สำหรับฟรี ปัจจุบัน
+  th-cat-ais-play
+  th-cat-true-nopro
+  th-cat-fbg
+  th-cat-dtac-line
+  th-cat-dtac-social
+  th-cat-dtac-happy-work
+  sg-cat-ais-play
+  sg-cat-fbg
+  sg-cat-true-nopro
+  sg-cat-dtac-line
+  sg-cat-dtac-social
+  sg-cat-dtac-happy-work
+
+ไฟล์สำหรับเช่า ปัจจุบัน
+  th-cat-ais-play
+  th-cat-fbg
+  th-cat-true-nopro
+  th-cat-dtac-line
+  th-cat-dtac-social
+  th-cat-dtac-happy-work
+
+ซิม&โปรที่ใช้งานได้
+sim ais = play
+sim dtac = line happy-work social
+sim true = fbg nopro
+",
+ 
+ 
+ 
+ 
+ 
+
+"latestVersion": "1.0.0",
+"latestVersionCode": "1",
+"url": "",
+ "releaseNotes": [
+ "-Demo"
+  ]
+}
