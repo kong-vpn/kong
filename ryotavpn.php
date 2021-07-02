d23567670f8f1cdd4d9266a51885c33e2e8c5461
kong-vpn <81752336+kong-vpn@users.noreply.github.com>
2021-07-02 23:32:14 +0700

    Add files via upload

---
+++ ryotavpn.php
@@ -0,0 +1,22 @@
+{"Version":"1.5",
+"Url":"https://github.com/kong-vpn/kong/raw/main/akiravpn.zip" ,
+"Changelog":" รายละเอียด
+
+•🌐ซิม&โปรที่ใช้งานได้
+•sim ais = play
+•sim dtac = line happy-work social
+•sim true = fbg nopro social
+",
+ 
+ 
+ 
+ 
+ 
+
+"latestVersion": "1.0.0",
+"latestVersionCode": "1",
+"url": "",
+ "releaseNotes": [
+ "-Demo"
+  ]
+}
