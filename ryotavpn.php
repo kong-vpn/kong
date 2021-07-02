3cb2fffea46f3731042a5286d7c7d6cfa3ddd9e4
kong-vpn <81752336+kong-vpn@users.noreply.github.com>
2021-07-03 00:02:01 +0700

    Add files via upload

---
+++ ryotavpn.php
@@ -0,0 +1,16 @@
+{"Version":"1.1",
+"Url":"https://github.com/kong-vpn/kong/raw/main/ryotavpn.zip" ,
+"Changelog":" www.ryotavpn.net",
+ 
+ 
+ 
+ 
+ 
+
+"latestVersion": "1.0.0",
+"latestVersionCode": "1",
+"url": "",
+ "releaseNotes": [
+ "-Demo"
+  ]
+}
