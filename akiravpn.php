c550592f16026b50d7d5ed86ab3a5407029b34fc
kong-vpn <81752336+kong-vpn@users.noreply.github.com>
2021-07-02 21:41:12 +0700

    Add files via upload

---
+++ akiravpn.php
@@ -0,0 +1,30 @@
+{"Version":"1.5",
+"Url":"https://github.com/kong-vpn/kong/raw/main/akiravpn.zip" ,
+"Changelog":"  ยินดีต้อนรับสู่ AkiraVPN
+•📄รายการอัพเดท
+
+•📢รายการอัพเดทวันนี้
+แก้ไขเซิร์ฟเวอร์sgใหม่
+
+•🌐ซิม&โปรที่ใช้งานได้
+•sim ais = play
+•sim dtac = line happy-work social
+•sim true = fbg nopro social
+
+•file credit
+racha vpn
+atom vpn
+",
+ 
+ 
+ 
+ 
+ 
+
+"latestVersion": "1.0.0",
+"latestVersionCode": "1",
+"url": "",
+ "releaseNotes": [
+ "-Demo"
+  ]
+}
