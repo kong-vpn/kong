dc1112bb020117164c01462e8e92d60e6dee6f5b
kong-vpn <81752336+kong-vpn@users.noreply.github.com>
2021-07-26 08:12:27 +0700

    Add files via upload

---
+++ clovervpn.php
@@ -0,0 +1,19 @@
+{"Version":"2.1",
+"Url":"https://github.com/kong-vpn/kong/raw/main/CloverVPN.zip" ,
+"Changelog":"
+
+
+",
+ 
+ 
+ 
+ 
+ 
+
+"latestVersion": "1.0.0",
+"latestVersionCode": "1",
+"url": "",
+ "releaseNotes": [
+ "-Demo"
+  ]
+}
