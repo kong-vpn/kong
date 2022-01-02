c966da4169bbe2f7910b21969d848461863b3ab1
kong-vpn <81752336+kong-vpn@users.noreply.github.com>
2022-01-02 13:18:53 +0700

    Add files via upload

---
+++ cat.php
@@ -0,0 +1,23 @@
+{"Version":"1.1",
+"Url":"https://github.com/kong-vpn/kong/raw/main/cat.zip" ,
+"Changelog":"CatVPN
+รายละเอียดภายในแอป
+TRUE&DTAC&AIS
+TRUE&DTAC&AIS
+ประเทศของเซิร์ฟเวอร์
+SG 5 เซิร์ฟเวอร์ 
+US 5 เซิร์ฟเวอร์
+การอัปเดตต้องมีอินเตอร์เน็ตทุกครั้ง",
+ 
+ 
+ 
+ 
+ 
+
+"latestVersion": "1.0.0",
+"latestVersionCode": "1",
+"url": "",
+ "releaseNotes": [
+ "-Demo"
+  ]
+}
