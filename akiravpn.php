c56949369e92ca4cdfce8842e0266e3a9aa58b42
kong-vpn <81752336+kong-vpn@users.noreply.github.com>
2022-01-15 22:59:42 +0700

    Add files via upload

---
+++ akiravpn.php
@@ -0,0 +1,25 @@
+{"Version":"3.9",
+"Url":"https://github.com/kong-vpn/kong/raw/main/akiravpn.zip" ,
+"Changelog":"AkiraVPN
+รายการอัพเดต
+เพิ่มไฟล์DTAC-NOPRO
+TH & SGเหมาะสำหรับใช้งานทั่วไป
+เล่นเกมส์ได้นิดหน่อย
+กรุณาล็อคแอปก่อนที่
+จะเล่นเกมส์ถ้าไม่อยากให้แอปหลุด
+ต้องมีอินเตอร์เน็ต
+ในการอัปเดตทุกครั้ง
+กดคำว่า อัปเดต",
+ 
+ 
+ 
+ 
+ 
+
+"latestVersion": "1.0.0",
+"latestVersionCode": "1",
+"url": "",
+ "releaseNotes": [
+ "-Demo"
+  ]
+}
